7ece96217bd4be6cff30707622bb5203906fa4cc
PripatOnGit <patil.priyu26@gmail.com>
2023-01-05 12:07:54 +0530

    Inteface ex2

---
+++ src/com/interfaces/ChildClass.java
@@ -0,0 +1,28 @@
+package com.interfaces;
+
+interface Interface2{
+	int a = 100; //consider as final/constant variable
+	void m1();
+	void m2();
+}
+
+abstract class Class1 implements Interface2{
+	public void m1() {
+		System.out.println("Methods1..");
+	}
+}
+
+abstract class Class2 extends Class1{
+	public void m2() {
+		System.out.println("Methods2..");
+	}
+}
+
+
+public class ChildClass extends Class2 {
+	public static void main(String[] args) {
+		ChildClass it2 = new ChildClass();
+		it2.m1();
+		it2.m2();
+	}
+}
